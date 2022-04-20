4644cbad6f4cd1bed3c801952419d4600004b8f1
Manel Márquez <manelmarquez@gignomai.es>
2022-04-21 00:31:33 +0200

    Initial commit.

---
+++ src/main/java/com/gignomai/javachangestutorial/JavaChangesTutorialApplication.java
@@ -0,0 +1,13 @@
+package com.gignomai.javachangestutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaChangesTutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaChangesTutorialApplication.class, args);
+	}
+
+}
